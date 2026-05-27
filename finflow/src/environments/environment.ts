5ac1270f0691ee1454fca88216f8fe565adc367b
Male29003 <nam2309200312345@gmail.com>
2026-05-28 01:34:16 +0700

    Update api url trong environment (da deploy)

--- finflow/src/environments/environment.ts
+++ finflow/src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
-  production: false,
-  apiUrl: 'https://localhost:7220/api/v1'
+   production: false,
+   apiUrl: 'https://finflow-d6eu.onrender.com/api/v1',
+   //apiUrl: 'https://localhost:7220/api/v1'
 };
